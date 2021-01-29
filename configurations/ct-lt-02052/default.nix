e83bae73d560814478f7d1db023dfb5576a1a455
Tristan Daniël Maat <tristan.maat@codethink.co.uk>
2021-01-29 20:35:03 +0000

    Add config for ct-lt-02052

---
+++ configurations/ct-lt-02052/default.nix
@@ -0,0 +1,31 @@
+{ pkgs, ... }:
+
+{
+  imports = [ ./hardware-configuration.nix ];
+
+  boot.initrd.luks.devices.root.device =
+    "/dev/disk/by-uuid/b3ac7dc6-cb0b-4350-bdfb-32329a5f61ff";
+
+  networking = {
+    hostName = "ct-lt-02052";
+    interfaces = {
+      enp0s31f6.useDHCP = true;
+      wlp0s20f3.useDHCP = true;
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
+    docker-compose # Better to keep it in lock-step with the system docker
+    fuse3 # For bst and related things
+  ];
+
+  # Virtualization is a lot more common at work...
+  virtualisation = {
+    docker.enable = true;
+    libvirtd.enable = true;
+  };
+  users.users.tlater.extraGroups = [ "docker" "libvirtd" ];
+
+  hardware.cpu.intel.updateMicrocode = true;
+  security.pki.certificates = [ (builtins.readFile ./codethink-wifi.cert) ];
+}
