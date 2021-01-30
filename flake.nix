8fcb31c38be4cba4fe990fb990215f698baa20e4
Tristan Daniël Maat <tm@tlater.net>
2021-01-30 01:05:28 +0000

    Update flake.nix formatting

--- flake.nix
+++ flake.nix
@@ -8,33 +8,34 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = inputs: {
-    nixosConfigurations = {
-      yui = inputs.nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          (import ./configurations)
-          (import ./configurations/yui)
-          (import ./configurations/bluetooth.nix)
-          (import ./configurations/wifi.nix)
-        ];
-        specialArgs = { inherit inputs; };
-      };
+  outputs = inputs:
+    {
+      nixosConfigurations = {
+        yui = inputs.nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            (import ./configurations)
+            (import ./configurations/yui)
+            (import ./configurations/bluetooth.nix)
+            (import ./configurations/wifi.nix)
+          ];
+          specialArgs = { inherit inputs; };
+        };
 
-      ct-lt-02052 = inputs.nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          (import ./configurations)
-          (import ./configurations/ct-lt-02052)
-          (import ./configurations/bluetooth.nix)
-          (import ./configurations/power.nix)
-          (import ./configurations/wifi.nix)
+        ct-lt-02052 = inputs.nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            (import ./configurations)
+            (import ./configurations/ct-lt-02052)
+            (import ./configurations/bluetooth.nix)
+            (import ./configurations/power.nix)
+            (import ./configurations/wifi.nix)
 
-          inputs.nixos-hardware.nixosModules.lenovo-thinkpad-t490
-        ];
-        specialArgs = { inherit inputs; };
+            inputs.nixos-hardware.nixosModules.lenovo-thinkpad-t490
+          ];
+          specialArgs = { inherit inputs; };
+        };
       };
-    };
     } // (inputs.flake-utils.lib.eachDefaultSystem (system:
       let pkgs = inputs.nixpkgs.legacyPackages.${system};
       in {
