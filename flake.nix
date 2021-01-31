7f79eb830fc896e556e65dfb3c34535f3dc78ba4
Tristan Daniël Maat <tm@tlater.net>
2021-01-31 19:24:47 +0000

    Refactor flake.nix for less duplication

--- flake.nix
+++ flake.nix
@@ -9,27 +9,37 @@
   };
 
   outputs = inputs:
-    {
+    let
+      # A helper function that removes the duplication of things that
+      # will be common across all hosts.
+      make-nixos-system = { nixpkgs, system, modules ? [ ] }:
+        let pkgs = nixpkgs.legacyPackages.${system};
+        in nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [ (import ./configurations) ] ++ modules;
+          extraModules = [ (import ./modules) ];
+          specialArgs = {
+            inherit inputs;
+            tlater-pkgs = (import ./pkgs { inherit pkgs; });
+          };
+        };
+
+    in {
       nixosConfigurations = {
-        yui = inputs.nixpkgs.lib.nixosSystem {
+        yui = make-nixos-system {
+          nixpkgs = inputs.nixpkgs;
           system = "x86_64-linux";
           modules = [
-            (import ./configurations)
             (import ./configurations/yui)
             (import ./configurations/bluetooth.nix)
             (import ./configurations/wifi.nix)
           ];
-          extraModules = [ (import ./modules) ];
-          specialArgs = {
-            inherit inputs;
-            tlater-pkgs = (import ./pkgs { pkgs = inputs.nixpkgs.legacyPackages."x86_64-linux"; });
-          };
         };
 
-        ct-lt-02052 = inputs.nixpkgs.lib.nixosSystem {
+        ct-lt-02052 = make-nixos-system {
+          nixpkgs = inputs.nixpkgs;
           system = "x86_64-linux";
           modules = [
-            (import ./configurations)
             (import ./configurations/ct-lt-02052)
             (import ./configurations/bluetooth.nix)
             (import ./configurations/power.nix)
@@ -37,14 +47,11 @@
 
             inputs.nixos-hardware.nixosModules.lenovo-thinkpad-t490
           ];
-          extraModules = [ (import ./modules) ];
-          specialArgs = {
-            inherit inputs;
-            tlater-pkgs = (import ./pkgs { pkgs = inputs.nixpkgs.legacyPackages."x86_64-linux"; });
-          };
         };
       };
-    } // (inputs.flake-utils.lib.eachDefaultSystem (system:
+    }
+    # Set up a "dev shell" that will work on all architectures.
+    // (inputs.flake-utils.lib.eachDefaultSystem (system:
       let pkgs = inputs.nixpkgs.legacyPackages.${system};
       in {
         devShell = pkgs.mkShell { buildInputs = with pkgs; [ nixfmt ]; };
